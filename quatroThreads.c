3c6af53557e3d90899372a4d5595e0036291fbcf
Henrique Yuji Isogai Yoneoka <Henriqueyuji8@gmail.com>
2025-05-12 01:26:29 +0000

    Criação dos códigos com 2, 3 e 4 threads

---
+++ quatroThreads.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define MAX_CHILDREN 2
+#define MAX_VALUE 100
+#define MAX_NODES 1000
+#define MAX_THREADS 4  // Limitando o número de threads a 4
+
+typedef struct {
+    double value;
+    int num_children;
+    int children[MAX_CHILDREN];
+} Node;
+
+typedef struct {
+    double sum;
+    int path[MAX_NODES];
+    int pathLength;
+} Result;
+
+typedef struct {
+    Node* tree;
+    int idx;
+    Result result;
+} ThreadArgs;
+
+Result computePaths(Node* tree, int idx); // Forward declaration
+
+void* threadFunc(void* arg) {
+    ThreadArgs* args = (ThreadArgs*)arg;
+    args->result = computePaths(args->tree, args->idx);
+    return NULL;
+}
+
+Result computePaths(Node* tree, int idx) {
+    Result res = {0, {0}, 0};
+
+    if (idx == -1) return res;
+
+    if (tree[idx].num_children == 0) {
+        res.sum = tree[idx].value;
+        res.path[res.pathLength++] = idx;
+        return res;
+    }
+
+    double maxSum = -1;
+    Result maxChildRes = {0};
+
+    pthread_t threads[MAX_THREADS];
+    ThreadArgs args[MAX_THREADS];
+    int numThreads = 0;
+
+    // Criação das threads, mas limitando o número de threads a MAX_THREADS
+    for (int i = 0; i < tree[idx].num_children && numThreads < MAX_THREADS; ++i) {
+        args[numThreads].tree = tree;
+        args[numThreads].idx = tree[idx].children[i];
+        pthread_create(&threads[numThreads], NULL, threadFunc, &args[numThreads]);
+        numThreads++;
+    }
+
+    // Espera o término e coleta os resultados
+    for (int i = 0; i < numThreads; ++i) {
+        pthread_join(threads[i], NULL);
+        if (args[i].result.sum > maxSum) {
+            maxSum = args[i].result.sum;
+            maxChildRes = args[i].result;
+        }
+    }
+
+    // Se ainda houver filhos restantes que não foram processados por threads
+    for (int i = numThreads; i < tree[idx].num_children; ++i) {
+        Result childRes = computePaths(tree, tree[idx].children[i]);
+        if (childRes.sum > maxSum) {
+            maxSum = childRes.sum;
+            maxChildRes = childRes;
+        }
+    }
+
+    res.sum = tree[idx].value + maxSum;
+    res.path[0] = idx;
+    for (int i = 0; i < maxChildRes.pathLength; ++i) {
+        res.path[i + 1] = maxChildRes.path[i];
+    }
+    res.pathLength = maxChildRes.pathLength + 1;
+
+    return res;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    Node* tree = (Node*)malloc(N * sizeof(Node));
+    for (int i = 0; i < N; ++i) {
+        scanf("%lf %d", &tree[i].value, &tree[i].num_children);
+        for (int j = 0; j < tree[i].num_children; ++j) {
+            scanf("%d", &tree[i].children[j]);
+            tree[i].children[j]--; // Ajusta para índice 0-based
+        }
+    }
+
+    Result finalRes = computePaths(tree, 0);
+    printf("Max Sum: %.2lf\n", finalRes.sum);
+    printf("Path: ");
+    for (int i = 0; i < finalRes.pathLength; ++i) {
+        printf("%d ", finalRes.path[i] + 1); // Ajusta de volta para índice 1-based
+    }
+    printf("\n");
+
+    free(tree);
+    return 0;
+}
